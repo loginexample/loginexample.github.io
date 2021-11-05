52ff06e9f135c1e2a8e5f6338cd17c010ce8f7f2
Callum <72660075+callumrawlinson@users.noreply.github.com>
2021-11-05 14:06:18 +0000

    Create double.js

---
+++ double.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    //$('#username').focus();
+
+    $('#submit').click(function() {
+
+        event.preventDefault(); // prevent PageReLoad
+
+       var ValidEmail = $('#username').val() === '9868callum'; // User validate
+var ValidPassword = $('#password').val() === 'password1'; // Password validate
+
+        if (ValidEmail === true && ValidPassword === true) { // if ValidEmail & ValidPassword
+            $('.valid').css('display', 'block');
+            window.location = "https://eirias.co.uk"; // go to home.html
+        }
+        else {
+            $('.error').css('display', 'block'); // show error msg
+        }
+    });
+});
