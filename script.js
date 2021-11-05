8cf81c7be1525bb8bb9ebd27f718e006d037eb30
Callum <72660075+callumrawlinson@users.noreply.github.com>
2021-11-05 13:41:13 +0000

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ $(document).ready(function() {
 
         event.preventDefault(); // prevent PageReLoad
 
-       var ValidEmail = $('#username').val() === 'invitado'; // User validate
-var ValidPassword = $('#password').val() === 'hgm2015'; // Password validate
+       var ValidEmail = $('#username').val() === '9868callum'; // User validate
+var ValidPassword = $('#password').val() === 'password1'; // Password validate
 
         if (ValidEmail === true && ValidPassword === true) { // if ValidEmail & ValidPassword
             $('.valid').css('display', 'block');
-            window.location = "http://arkev.com"; // go to home.html
+            window.location = "http://eirias.co.uk"; // go to home.html
         }
         else {
             $('.error').css('display', 'block'); // show error msg
