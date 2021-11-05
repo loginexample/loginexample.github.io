0133f340a01be372ff11e9f3b56c1f96bb2b9181
Callum <72660075+callumrawlinson@users.noreply.github.com>
2021-11-05 14:23:29 +0000

    Update script1.js

--- script1.js
+++ script1.js
@@ -5,10 +5,9 @@ $(document).ready(function() {
 
         event.preventDefault(); // prevent PageReLoad
 
-       var ValidEmail = $('#username').val() === 'callum'; // User validate
 var ValidPassword = $('#password').val() === '6415'; // Password validate
 
-        if (ValidEmail === true && ValidPassword === true) { // if ValidEmail & ValidPassword
+        if (ValidPassword === true) { // if ValidEmail & ValidPassword
             $('.valid').css('display', 'block');
             window.location = "https://eirias.co.uk"; // go to home.html
         }
