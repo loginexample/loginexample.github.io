e5693327fa31c706c3b140592e1ce06585d1a09e
Callum <72660075+callumrawlinson@users.noreply.github.com>
2021-11-05 14:16:38 +0000

    Update double.js

--- double.js
+++ double.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
 
         event.preventDefault(); // prevent PageReLoad
 
-       var ValidEmail = $('#username').val() === '9868callum'; // User validate
-var ValidPassword = $('#password').val() === 'password2'; // Password validate
+       var ValidEmail = $('#username').val() === 'callum'; // User validate
+var ValidPassword = $('#password').val() === '6415'; // Password validate
 
         if (ValidEmail === true && ValidPassword === true) { // if ValidEmail & ValidPassword
             $('.valid').css('display', 'block');
