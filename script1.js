f591b8459504fc10bb4c9f3d816ba241755b6901
Callum <72660075+callumrawlinson@users.noreply.github.com>
2021-11-08 12:52:27 +0000

    Update script1.js

--- script1.js
+++ script1.js
@@ -10,7 +10,7 @@ var ValidPassword = $('#password').val() === '6415'; // Password validate
 
         if (ValidEmail === true && ValidPassword === true) { // if ValidEmail & ValidPassword
             $('.valid').css('display', 'block');
-            window.location = "https://loginexample.github.io/co"; // go to home.html
+            window.location = "https://mcduk.reflexisinc.co.uk/kernel/views/authenticate/W/MCDUK.view"; // go to home.html
         }
         else {
             $('.error').css('display', 'block'); // show error msg
